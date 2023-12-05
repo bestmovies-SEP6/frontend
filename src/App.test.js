e24fe0a1c19a32040d2e89603489682244454144
Himal sharma <himal28924@gmail.com>
2023-12-05 16:53:48 +0100

    Revert "Trying to pass test file"
    This reverts commit d6514a817cc0cf5b4a48dd4ce3f6a3e06fd58586.

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,3 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+// import { render, screen } from '@testing-library/react';
+// import App from './ui/pages/home/App';
 
-test('App component renders', () => {
-    render(<App />);
-    const linkElement = screen.getByText(/MovieCard/i);
-    expect(linkElement).toBeInTheDocument();
-});
-jest.mock('react-slick', () => ({
-    // Mock the Slider and other components you use from react-slick
-    Slider: ({ children }) => <div>{children}</div>,
-    // Add other components as needed
-}));
